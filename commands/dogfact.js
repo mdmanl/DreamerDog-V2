faa70bb39ecbea44417aa008c153e70aedd028a2
Joey van Kouwenberg <joey@jvk.ovh>
2020-09-08 00:15:42 +0200

    init

---
+++ commands/dogfact.js
@@ -0,0 +1,18 @@
+const axios = require('axios');
+const Discord = require('discord.js');
+
+module.exports = {
+    name: 'dogfact',
+    aliases: ['fact'],
+	cooldown: 5,
+	description: 'Shows a random dog fact.',
+	async execute(message) {
+        let getDogfact = async () => {
+            let response = await axios.get('https://some-random-api.ml/facts/dog');
+            let Dogfact = response.data;
+            return Dogfact;
+          }
+          let DogfactValue = await getDogfact();
+          message.channel.send(`${DogfactValue.fact}`)   
+    },
+};
