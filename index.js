28b6f86806fc0a551ddd310baeaa75f6e16652f0
Joey van Kouwenberg <joey@jvk.ovh>
2020-09-22 15:53:15 +0200

    Changed default cooldown to 0

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ for (const file of commandFiles) {
 const cooldowns = new Discord.Collection();
 
 client.once('ready', () => {
-	client.user.setActivity('HaiX.best', { type: 'WATCHING' })
+	client.user.setActivity('www.HaiX.best', { type: 'WATCHING' })
 	console.log('DreamerDog initialized & ready!');
 });
 
