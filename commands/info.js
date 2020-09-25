21f8c9102077092638e0e6f7aa067957ed04119b
Joey van Kouwenberg <joey@jvk.ovh>
2020-09-25 12:57:55 +0200

    Init

--- commands/info.js
+++ commands/info.js
@@ -3,6 +3,6 @@ module.exports = {
 	cooldown: 5,
 	description: 'Shows information about the bot and version.',
 	execute(message) {
-		message.channel.send(`Sup dog! I am DreamerDog 2.0 <a:pepesaber:752499405814497342>\nI am a private bot for **Haix** created by **MDMA** with Node.js, JavaScript, PHP and lots of love!`);
+		message.channel.send(`Sup dog! I am DreamerDog 2.0 <a:pepesaber:752499405814497342>\nI am a private bot for **Haix** created by **MDMA** with Node.js, JavaScr0pt, PHP and lots of love!`);
 	},
 };
