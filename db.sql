837a048e73e7f8a0dae302f0ab49a85a93c82938
Joey van Kouwenberg <joey@jvk.ovh>
2020-10-15 01:02:47 +0200

    Published db.sql

---
+++ db.sql
@@ -0,0 +1,19 @@
+CREATE TABLE warnings 
+(
+  ID int(11) NOT NULL AUTO_INCREMENT,
+  memberID varchar(50) NOT NULL DEFAULT '0',
+  reason varchar(200) NOT NULL DEFAULT '0',
+  expiryDate bigint(255) DEFAULT NULL,
+  admin varchar(200) NOT NULL DEFAULT '0',
+  activeWarns varchar(5) NOT NULL DEFAULT '0',
+  PRIMARY KEY (ID)
+);
+
+CREATE TABLE users 
+(
+  id int(11) NOT NULL AUTO_INCREMENT,
+  memberID varchar(50) NOT NULL DEFAULT '0',
+  bio varchar(50) NOT NULL DEFAULT '0',
+  hideBio varchar(10) NOT NULL DEFAULT '0',
+  PRIMARY KEY (ID)
+);
