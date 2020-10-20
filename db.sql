712b80574414a59a8c5496733c1062da6e944a56
mdmanl <70902089+mdmanl@users.noreply.github.com>
2020-10-20 16:29:21 +0200

    Update db.sql

--- db.sql
+++ db.sql
@@ -17,3 +17,12 @@ CREATE TABLE users
   hideBio varchar(10) NOT NULL DEFAULT '0',
   PRIMARY KEY (ID)
 );
+
+CREATE TABLE stats 
+(
+  id int(11) NOT NULL AUTO_INCREMENT,
+  warns varchar(200) NOT NULL DEFAULT '0',
+  bans varchar(200) NOT NULL DEFAULT '0',
+  kicks varchar(200) NOT NULL DEFAULT '0',
+  PRIMARY KEY (ID)
+);
