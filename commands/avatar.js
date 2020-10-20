6b3e93e410d4e37e3f70c1fd2e9e6ac047e99ca7
Joey van Kouwenberg <joey@jvk.ovh>
2020-10-20 16:25:48 +0200

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,16 +1,38 @@
+const Discord = require("discord.js");
+
 module.exports = {
 	name: 'avatar',
 	cooldown: 10,
 	description: 'Get the avatar URL of the tagged user, or your own avatar.',
 	execute(message) {
-		if (!message.mentions.users.size) {
-			return message.channel.send(`Your avatar: ${message.author.displayAvatarURL({ dynamic: true })}`);
+
+		let member = message.mentions.members.first();
+
+		if (!member) {
+			
+			var eAvatar = new Discord.MessageEmbed()
+			.setColor('#73e600')
+			.setAuthor(`${message.author.username}'s avatar`, message.author.displayAvatarURL(), '')
+			.setImage(message.author.displayAvatarURL({ dynamic: true, size: 1024 }))
+			.setTimestamp()
+			.setFooter(`Requested by ${message.author.username}`);
+	
+		message.channel.send(eAvatar);
+
+		} else {
+
+			var eAvatar = new Discord.MessageEmbed()
+			.setColor('#73e600')
+			.setAuthor(`${member.user.username}'s avatar`, member.user.displayAvatarURL(), '')
+			.setImage(member.user.displayAvatarURL({ dynamic: true, size: 1024 }))
+			.setTimestamp()
+			.setFooter(`Requested by ${message.author.username}`);
+	
+		message.channel.send(eAvatar);
+
 		}
 
-		const avatarList = message.mentions.users.map(user => {
-			return `${user.username}'s avatar: ${user.displayAvatarURL({ dynamic: true })}`;
-		});
+		
 
-		message.channel.send(avatarList);
 	},
 };
