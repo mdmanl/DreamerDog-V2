9eb269a299d84de14500e8f16cc03f4f1fc7422e
Joey van Kouwenberg <joey@jvk.ovh>
2020-10-22 00:45:57 +0200

    Showing active warns

--- commands/stats.js
+++ commands/stats.js
@@ -8,13 +8,17 @@ module.exports = {
 
         con.query(`SELECT * FROM stats WHERE id = '1'` , (err , rows) => {
 
-            if (err) throw err;
+			if (err) throw err;
+			
+			activeWarns = rows[0].warns - rows[0].unwarns;
 
             var statsEmbed = new Discord.MessageEmbed()
 			    .setColor('#73e600')
-			    .addField('Warns:', `${rows[0].warns}`, true)
+				.addField('Warns:', `${rows[0].warns}`, true)
+				.addField('Active Warns:', `${activeWarns}`, true)
 			    .addField('Kicks:', `${rows[0].kicks}`, true)
-			    .addField('Bans:', `${rows[0].bans}`, true)
+				.addField('Bans:', `${rows[0].bans}`, true)
+				.addField('Members:', `${message.guild.memberCount}`, false)
                 .setAuthor(`DreamerDog Stats since V2`)
                 .setTimestamp()
 			    .setFooter(`Requested by ${message.author.username}`)
